667d184f169065a87a482dd73c5780536e965e91
parmenasoares <parmenas.oliveira@gmail.com>
2026-08-14 18:41:40 -0300

    fix: load registration styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
+import './styles/auth-flow.css'
+import './styles/seller-registration.css'
 import './styles/address.css'
 
 createRoot(document.getElementById('root')).render(
