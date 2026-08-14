95dfd1d9bfa043b83eafd6463c5417aab897f964
parmenasoares <parmenas.oliveira@gmail.com>
2026-08-14 16:36:39 -0300

    chore: wire address lookup styles into app entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
-import './styles/seller-registration.css'
+import './styles/address.css'
 
-createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
